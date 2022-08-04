98b267a97829931dc423ede9b3a1ec9599e1b985
640610645 <nontapan_chanadee@cmu.ac.th>
2022-08-04 14:13:25 +0700

    quiz2

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+const imgProfile = document.querySelector("#img-profile");
+const pName = document.querySelector("#p-name");
+const pAddress = document.querySelector("#p-address");
+const pEmail = document.querySelector("#p-email");
+const btnRandom = document.querySelector("#btn-random");
+
 async function callApi() {
   const resp = await axios.get("https://randomuser.me/api/");
+  imgProfile.src = resp.data.result.picture;
+  pName = resp.data.result.picture;
+  pAddress = resp.data.picture;
+  pEmail = resp.data.picture;
 }
+
+btnRandom.onclick = async () => {
+  btnRandom.innerText = "Loading...";
+  btnRandom.disabled = true;
+
+  const resp = await axios.get("https://dog.ceo/api/breeds/image/random");
+};
